d2274db80b3ee3a930ae6a751ed4b525d7eb8b42
m-emilio <contact.emilio1@gmail.com>
2025-09-12 15:56:44 -0400

    golden

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -65,7 +65,8 @@ function App() {
       diffuse: 0.8,
       mapSamples: 16000,
       mapBrightness: 6,
-      baseColor: [0.3, 0.3, 0.3],
+      //      baseColor: [0.3, 0.3, 0.3], // dark gray
+  baseColor: [239/255, 191/255, 4/255], // #efbf04 gold
       markerColor: [0.8, 0.1, 0.1],
       glowColor: [0.2, 0.2, 0.2],
       markers: [],
